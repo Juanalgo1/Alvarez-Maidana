69cbd63d076680322ced5260344aed68bd9230da
Juan <jbalvarez@estudiantes.unsam.edu.ar>
2024-10-12 17:45:10 -0300

    Checkpoint

--- Alvarez-Maidana.py
+++ Alvarez-Maidana.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 
 path_file = "students.csv"
 dataframe = pd.read_csv(path_file) 
-dataframe.dropna(subset = "Avg_Study_time_per_day", inplace = True)
-dataframe.drop(columns = ["Timestamp", "Username"], inplace = True)
 ##print(dataframe)
 print(dataframe.describe())
+print(dataframe.corr())
