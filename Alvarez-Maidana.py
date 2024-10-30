f2dc8b49ca3a4515ecc9790cc034817aefefebac
Juan <jbalvarez@estudiantes.unsam.edu.ar>
2024-10-30 14:22:46 -0300

    Separo gráficos de código principal

--- Alvarez-Maidana.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-path_file = "students.csv"
-dataframe = pd.read_csv(path_file) 
-##print(dataframe)
-print(dataframe.describe())
-print(dataframe.corr())
